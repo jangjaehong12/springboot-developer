a063a301a7fe28f0b4f3c41b0ac095f004ecf110
장재홍 <wwg8947@g.yju.ac.kr>
2026-06-15 18:36:24 +0900

    JWT Token-based Auth

--- src/main/java/me/jhjang/springdeveloper/config/jwt/JwtProperties.java
+++ src/main/java/me/jhjang/springdeveloper/config/jwt/JwtProperties.java
@@ -1,6 +1,5 @@
 package me.jhjang.springdeveloper.config.jwt;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,9 +8,8 @@ import org.springframework.stereotype.Component;
 @Setter
 @Getter
 @Component
-@ConfigurationProperties("jwt") //application.yml 의 jwt 프로퍼티 값을 가져와서 사용하기 위한 애너테이션
+@ConfigurationProperties(prefix = "spring.jwt")
 public class JwtProperties {
     private String issuer;
     private String secretKey;
-
 }
